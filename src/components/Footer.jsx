613d14b26cdd97dc715a3eb321943052b5c3cdd0
Ltxstudio <ltxstudio.info@gmail.com>
2024-11-20 13:31:48 +0600

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="bg-gradient-to-r from-blue-500 to-purple-600 dark:from-gray-800 dark:to-gray-700 text-white p-4 text-center">
+      <p>&copy; 2024 My Mobile App. All rights reserved.</p>
+      <p>Follow us on social media for updates.</p>
+    </footer>
+  );
+}
+
+export default Footer;
